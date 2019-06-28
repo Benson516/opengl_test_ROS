8a496e91b09d0de2015a278cec041d4670b74a24
Chun-Feng Huang <benson516@hotmail.com>
2019-06-28 17:32:23 +0800

    Add tagBoundingBox, modify the Text2D

---
+++ opengl_test/include/RenderingModels/rmL2TagBoundingBox2D.cpp
@@ -0,0 +1,267 @@
+#include "rmL2TagBoundingBox2D.h"
+
+// Predefined colors
+//-------------------------------------------//
+#define NUM_OBJ_CLASS 8
+#define color_normalize_factor  (1.0f/255.0f)
+glm::vec3 default_class_color(50, 50, 50);
+glm::vec3 obj_class_colors[] = {
+    glm::vec3(50, 50, 255), // person
+    glm::vec3(255, 153, 102), // bicycle
+    glm::vec3(153, 255, 255), // car
+    glm::vec3(255, 153, 127), // motorbike
+    glm::vec3(255, 255, 0), // not showing aeroplane
+    glm::vec3(102, 204, 255), // bus
+    glm::vec3(255, 255, 100), // not showing train
+    glm::vec3(255, 153, 102), // truck
+    glm::vec3(50, 50, 50) // default
+};
+glm::vec3 get_obj_class_color(int obj_class_in){
+    if (obj_class_in < NUM_OBJ_CLASS){
+        return ( obj_class_colors[obj_class_in] * color_normalize_factor );
+    }
+    return ( default_class_color * color_normalize_factor );
+}
+//-------------------------------------------//
+
+// Box vertex index
+namespace rmLidarBoundingBox_ns{
+    const GLuint box_idx_data[] = {
+        0,1,
+        1,2,
+        2,3,
+        3,0
+    };
+}
+
+
+
+rmL2TagBoundingBox2D::rmL2TagBoundingBox2D(
+    std::string _path_Assets_in,
+    int _ROS_topic_id_in,
+    bool is_perspected_in,
+    bool is_moveable_in
+):
+    is_perspected(is_perspected_in),
+    is_moveable(is_moveable_in),
+    _ROS_topic_id(_ROS_topic_id_in)
+{
+    _path_Shaders_sub_dir += "BoundingBox2D/";
+    init_paths(_path_Assets_in);
+    //
+    _num_vertex_idx_per_box = 4*2; // 6*(3*2);
+    _num_vertex_per_box = 4; // 8;
+    _max_num_box = 1000;
+    _max_num_vertex_idx = _max_num_box*(long long)(_num_vertex_idx_per_box);
+    _max_num_vertex = _max_num_box*(long long)(_num_vertex_per_box);
+    //
+    generate_box_template();
+    //
+	Init();
+}
+void rmL2TagBoundingBox2D::Init(){
+    //
+	_program_ptr.reset( new ShaderProgram() );
+    // Load shaders
+    // VS
+    if (is_perspected){
+        _program_ptr->AttachShader(get_full_Shader_path("BoundingBox2D.vs.Perspected.glsl"), GL_VERTEX_SHADER);
+    }else{
+        if (is_moveable){
+            _program_ptr->AttachShader(get_full_Shader_path("BoundingBox2D.vs.Moveable.glsl"), GL_VERTEX_SHADER);
+        }else{ // Background
+            _program_ptr->AttachShader(get_full_Shader_path("BoundingBox2D.vs.Background.glsl"), GL_VERTEX_SHADER);
+        }
+    }
+    _program_ptr->AttachShader(get_full_Shader_path("BoundingBox2D.fs.glsl"), GL_FRAGMENT_SHADER);
+    // Link _program_ptr
+	_program_ptr->LinkProgram();
+    //
+
+
+
+
+    // Init model matrices
+	m_shape.model = glm::mat4(1.0);
+    attach_pose_model_by_model_ref_ptr(m_shape.model); // For adjusting the model pose by public methods
+
+    // Cache uniform variable id
+    //----------------------------------------//
+    if (is_perspected){
+        uniforms.proj_matrix = glGetUniformLocation(_program_ptr->GetID(), "proj_matrix");
+        uniforms.mv_matrix = glGetUniformLocation(_program_ptr->GetID(), "mv_matrix");
+    }else if (is_moveable){
+        uniforms.mv_matrix = glGetUniformLocation(_program_ptr->GetID(), "mv_matrix");
+    }
+    //----------------------------------------//
+
+
+    //Load model to shader _program_ptr
+	LoadModel();
+
+}
+void rmL2TagBoundingBox2D::LoadModel(){
+    glGenVertexArrays(1, &m_shape.vao);
+	glBindVertexArray(m_shape.vao);
+
+    glGenBuffers(1, &m_shape.vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, m_shape.vbo);
+    glBufferData(GL_ARRAY_BUFFER, _max_num_vertex * sizeof(vertex_p_c_2D), NULL, GL_DYNAMIC_DRAW); // test, change to dynamic draw to assign point cloud
+    // Directly assign data to memory of GPU
+    //--------------------------------------------//
+	vertex_p_c_2D * vertex_ptr = (vertex_p_c_2D *)glMapBufferRange(GL_ARRAY_BUFFER, 0, _max_num_vertex * sizeof(vertex_p_c_2D), GL_MAP_WRITE_BIT | GL_MAP_INVALIDATE_BUFFER_BIT);
+	size_t _j = 0;
+    float box_size = 2.0;
+	for (size_t i = 0; i < _max_num_vertex; i++)
+	{
+		vertex_ptr[i].position[0] = box_size * box_template[_j].position[0] + 1*box_size*(i/_num_vertex_per_box);
+		vertex_ptr[i].position[1] = box_size * box_template[_j].position[1] + 1*box_size*(i/_num_vertex_per_box);
+		vertex_ptr[i].color[0] = 1.0f; //
+		vertex_ptr[i].color[1] = 1.0f; //
+		vertex_ptr[i].color[2] = 1.0f; //
+        _j++;
+        _j = _j%_num_vertex_per_box;
+	}
+	glUnmapBuffer(GL_ARRAY_BUFFER);
+    //--------------------------------------------//
+
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(vertex_p_c_2D), NULL);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(vertex_p_c_2D), (void *)sizeof(glm::vec2));
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+
+    // ebo
+    glGenBuffers(1, &m_shape.ebo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_shape.ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, _max_num_vertex_idx * sizeof(GLuint), NULL, GL_STATIC_DRAW);
+    // Directly assign data to memory of GPU
+    //--------------------------------------------//
+	GLuint * _idx_ptr = (GLuint *)glMapBufferRange(GL_ELEMENT_ARRAY_BUFFER, 0, _max_num_vertex_idx * sizeof(GLuint), GL_MAP_WRITE_BIT | GL_MAP_INVALIDATE_BUFFER_BIT);
+	size_t _idx_idx = 0;
+    long long _offset_box = 0;
+	for (size_t i = 0; i < _max_num_vertex_idx; i++)
+	{
+        _idx_ptr[i] = rmLidarBoundingBox_ns::box_idx_data[_idx_idx] + _offset_box;
+        // std::cout << "_idx_ptr[" << i << "] = " << _idx_ptr[i] << "\n";
+        _idx_idx++;
+        if (_idx_idx >= _num_vertex_idx_per_box){
+            _idx_idx = 0;
+            _offset_box += _num_vertex_per_box;
+        }
+	}
+	glUnmapBuffer(GL_ELEMENT_ARRAY_BUFFER);
+    m_shape.indexCount = _max_num_vertex_idx; //  1 * _num_vertex_idx_per_box; // ;
+    //--------------------------------------------//
+
+
+
+
+}
+void rmL2TagBoundingBox2D::Update(float dt){
+    // Update the data (buffer variables) here
+}
+void rmL2TagBoundingBox2D::Update(ROS_INTERFACE &ros_interface){
+    // Update the data (buffer variables) here
+
+    // test, use transform
+    ros::Time msg_time;
+    bool _result = ros_interface.get_void_message( _ROS_topic_id, &msg_out_ptr, msg_time);
+
+    if (_result){
+        update_GL_data();
+    }
+
+    // Move in 3D space
+    if (  is_perspected && ros_interface.is_topic_got_frame(_ROS_topic_id)){
+        // Note: We get the transform update even if there is no new content in for maximum smoothness
+        //      (the tf will update even there is no data)
+        bool tf_successed = false;
+        glm::mat4 _model_tf = ROStf2GLMmatrix(ros_interface.get_tf(_ROS_topic_id, tf_successed, false));
+        // glm::mat4 _model_tf = ROStf2GLMmatrix(ros_interface.get_tf(_ROS_topic_id, tf_successed, true, msg_time));
+        // m_shape.model = _model_tf;
+        set_pose_modle_ref_by_world(_model_tf);
+        // Common::print_out_mat4(_model_tf);
+    }
+
+}
+
+void rmL2TagBoundingBox2D::Update(ROS_API &ros_api){
+    // Update the data (buffer variables) here
+    // test, use transform
+    ros::Time msg_time;
+    bool _result = false;
+    /*
+    // Scops for any_ptr
+    {
+        boost::any any_ptr;
+        _result = ros_api.get_any_message( _ROS_topic_id, any_ptr, msg_time );
+        if (_result){
+            std::shared_ptr< msgs::CamObj > *_ptr_ptr = boost::any_cast< std::shared_ptr< msgs::CamObj > >( &any_ptr );
+            msg_out_ptr = *_ptr_ptr;
+        }
+    }// end Scops for any_ptr
+    */
+    _result = ros_api.get_message(_ROS_topic_id, msg_out_ptr, msg_time);
+
+    if (_result){
+        update_GL_data();
+    }
+
+
+    // Move in 3D space
+    if ( is_perspected && ros_api.ros_interface.is_topic_got_frame(_ROS_topic_id)){
+        // Get tf
+        bool tf_successed = false;
+        glm::mat4 _model_tf = ROStf2GLMmatrix(ros_api.get_tf(_ROS_topic_id, tf_successed));
+        set_pose_modle_ref_by_world(_model_tf);
+        // end Get tf
+
+        /*
+        ROS_INTERFACE &ros_interface = ros_api.ros_interface;
+        // Note: We get the transform update even if there is no new content in for maximum smoothness
+        //      (the tf will update even there is no data)
+        bool tf_successed = false;
+        glm::mat4 _model_tf = ROStf2GLMmatrix(ros_interface.get_tf(_ROS_topic_id, tf_successed, false));
+        // glm::mat4 _model_tf = ROStf2GLMmatrix(ros_interface.get_tf(_ROS_topic_id, tf_successed, true, msg_time));
+        // m_shape.model = _model_tf;
+        set_pose_modle_ref_by_world(_model_tf);
+        // Common::print_out_mat4(_model_tf);
+        */
+    }
+}
+
+
+void rmL2TagBoundingBox2D::Render(std::shared_ptr<ViewManager> _camera_ptr){
+
+    glBindVertexArray(m_shape.vao);
+
+	_program_ptr->UseProgram();
+
+    if (is_perspected){
+        //
+        // m_shape.model = translateMatrix * rotateMatrix * scaleMatrix;
+        // The transformation matrices and projection matrices
+        glUniformMatrix4fv(uniforms.mv_matrix, 1, GL_FALSE, value_ptr( get_mv_matrix(_camera_ptr, m_shape.model) ));
+        glUniformMatrix4fv(uniforms.proj_matrix, 1, GL_FALSE, value_ptr(_camera_ptr->GetProjectionMatrix()));
+    }else{
+        if (is_moveable){
+            // Note: the rotation is mainly for z-axis rotation
+            // Note 2: The tranalation/rotation/scale is based on the "center" of the image
+            // m_shape.model = translateMatrix * rotateMatrix * scaleMatrix;
+            glUniformMatrix4fv(uniforms.mv_matrix, 1, GL_FALSE, value_ptr( m_shape.model ));
+        }else{
+            // background
+            // Nothing, for saving computation
+        }
+    }
+
+    // Setting
+    // glLineWidth(5.0);
+
+    // Draw the element according to ebo
+    // glDrawElements(GL_TRIANGLES, m_shape.indexCount, GL_UNSIGNED_INT, 0);
+    glDrawElements(GL_LINES, m_shape.indexCount, GL_UNSIGNED_INT, 0);
+    // glDrawArrays(GL_TRIANGLES, 0, 3*5); // draw part of points
+    //--------------------------------//
+    _program_ptr->CloseProgram();
+}
--- opengl_test/include/RenderingModels/rmText2D.cpp
+++ opengl_test/include/RenderingModels/rmText2D.cpp
@@ -4,9 +4,13 @@
 void *font2D = GLUT_BITMAP_TIMES_ROMAN_24;
 void *fonts2D[] =
 {
-  GLUT_BITMAP_9_BY_15,
-  GLUT_BITMAP_TIMES_ROMAN_10,
-  GLUT_BITMAP_TIMES_ROMAN_24
+    GLUT_BITMAP_8_BY_13,
+    GLUT_BITMAP_9_BY_15,
+    GLUT_BITMAP_TIMES_ROMAN_10,
+    GLUT_BITMAP_TIMES_ROMAN_24,
+    GLUT_BITMAP_HELVETICA_10,
+    GLUT_BITMAP_HELVETICA_12,
+    GLUT_BITMAP_HELVETICA_18
 };
 
 
@@ -17,17 +21,12 @@ rmText2D::rmText2D()
 }
 void rmText2D::Init(){
     //
-    // Current text
-    text_current = "";
 
     //Load model to shader _program_ptr
 	LoadModel();
 
 }
 void rmText2D::LoadModel(){
-
-
-
 
 }
 void rmText2D::Update(float dt){
@@ -41,14 +40,43 @@ void rmText2D::Update(ROS_API &ros_api){
 }
 void rmText2D::Render(std::shared_ptr<ViewManager> _camera_ptr){
     // test
-    static int _count = 0;
+    // static int _count = 0;
     //
     glUseProgram(0); // Program 0: OpenGL ver1.0
-    selectFont2D(2);
-    text2D_output(0.5,0.8, "This is 2D text: " + std::to_string(_count++) );
+    // selectFont2D(2);
+    // text2D_output(0.5,0.8, "This is 2D text: " + std::to_string(_count++) );
+
+    for (size_t i=0; i < text2D_buffer.size(); ++i){
+        _draw_one_text2D(_camera_ptr, text2D_buffer.front() );
+        text2D_buffer.pop();
+    }
 }
 
 
+// Different draw methods
+//--------------------------------------------------------//
+void rmText2D::_draw_one_text2D(std::shared_ptr<ViewManager> &_camera_ptr, text2D_data &_data_in){
+    selectFont2D(3);
+
+    int _line_count = 1;
+    int _max_word_per_line = 0;
+    int _word_per_line = 0;
+    // Counting lines
+    for (size_t i = 0; i < string_in.size(); i++) {
+      if (string_in[i] == '\n'){
+          _line_count++;
+          _word_per_line = 0;
+      }else{
+          _word_per_line++;
+      }
+      if (_word_per_line > _max_word_per_line){
+          _max_word_per_line = _word_per_line;
+      }
+    }
+    //
+    text2D_output(_data_in.position_2D.x, _data_in.position_2D.y, _data_in.text);
+}
+//--------------------------------------------------------//
 
 //
 void rmText2D::selectFont2D(int newfont){
