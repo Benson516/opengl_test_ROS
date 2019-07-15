0ba388513896d9ccc0ecc1b8de0ba2e7fad4c1bb
Chun-Feng Huang <benson516@hotmail.com>
2019-07-15 13:49:39 +0800

    Move the to_string_p to all_header.h and modify the precision in showing fps

--- opengl_test/include/RenderingModels/rmText3D_v2.h
+++ opengl_test/include/RenderingModels/rmText3D_v2.h
@@ -25,15 +25,7 @@ Note 2:
 //------------------------------//
 */
 
-// to_string_with_precision
-template <typename T>
-std::string to_string_p(const T a_value, const int n = 6)
-{
-    std::ostringstream out;
-    out.precision(n);
-    out << std::fixed << a_value;
-    return out.str();
-}
+
 
 
 // atlas
--- opengl_test/src/visualizer2_main.cpp
+++ opengl_test/src/visualizer2_main.cpp
@@ -117,8 +117,10 @@ vec2			m_screenSize;
 int				m_currentCamera;
 int             m_currentCamera_old;
 float			m_zoom = 3.0f;
-float			m_fps_d = 0;
-std::vector<float> m_fps_topic;
+// float			m_fps_d = 0;
+std::string     m_fps_d_str = "0.0";
+// std::vector<float> m_fps_topic;
+std::vector<std::string> m_fps_topic_str;
 unsigned int	m_frames = 0;
 unsigned int    m_currentTime = 0;
 unsigned int    m_timebase = 0;
@@ -188,14 +190,17 @@ void setupGUI()
 	TwDefine(" Properties fontsize='3' color='0 0 0' alpha=180 ");  // http://anttweakbar.sourceforge.net/doc/tools:anttweakbar:twbarparamsyntax
 
     // FPS of display
-	TwAddVarRO(bar_1_ptr, "fps_d", TW_TYPE_FLOAT, &m_fps_d, " label='FPS-display' help='Frame Per Second(FPS)' ");
+    // TwAddVarRO(bar_1_ptr, "fps_d", TW_TYPE_FLOAT, &m_fps_d, " label='FPS-display' help='Frame Per Second(FPS)' ");
+	TwAddVarRO(bar_1_ptr, "fps_d", TW_TYPE_STDSTRING, &m_fps_d_str, " label='FPS-display' help='Frame Per Second(FPS)' ");
     //
     TwAddSeparator(bar_1_ptr, "Sep1", "");
     // FPS of camera
     ros_api.update();
-    m_fps_topic.resize( ros_api.ros_interface.get_count_of_all_topics(), 0.0f);
+    // m_fps_topic.resize( ros_api.ros_interface.get_count_of_all_topics(), 0.0f);
+    m_fps_topic_str.resize( ros_api.ros_interface.get_count_of_all_topics(), "0.0");
     for (int topic_idx = int(MSG_ID::camera_front_right); topic_idx < ros_api.ros_interface.get_count_of_all_topics(); ++topic_idx ){
-        TwAddVarRO(bar_1_ptr, ("fps_" + std::to_string(topic_idx)).c_str(), TW_TYPE_FLOAT, &(m_fps_topic[topic_idx]), (" label='FPS-" + ros_api.ros_interface.get_topic_name(topic_idx) + "' help='Frame Per Second(FPS)' ").c_str() );
+        // TwAddVarRO(bar_1_ptr, ("fps_" + std::to_string(topic_idx)).c_str(), TW_TYPE_FLOAT, &(m_fps_topic[topic_idx]), (" label='FPS-" + ros_api.ros_interface.get_topic_name(topic_idx) + "' help='Frame Per Second(FPS)' ").c_str() );
+        TwAddVarRO(bar_1_ptr, ("fps_" + std::to_string(topic_idx)).c_str(), TW_TYPE_STDSTRING, &(m_fps_d_str[topic_idx]), (" label='FPS-" + ros_api.ros_interface.get_topic_name(topic_idx) + "' help='Frame Per Second(FPS)' ").c_str() );
     }
     //
     TwAddSeparator(bar_1_ptr, "Sep2", "");
@@ -256,10 +261,12 @@ void My_Display()
 {
     // FPS of the display
     fps_display.stamp();
-    m_fps_d = fps_display.fps;
+    // m_fps_d = fps_display.fps;
+    m_fps_d_str = all_header::to_string_p(fps_display.fps, 1);
     // FPS for topics
-    for (size_t i=0; i < m_fps_topic.size(); ++i){
-        m_fps_topic[i] = ros_api.fps_list[i].fps;
+    for (size_t i=0; i < m_fps_d_str.size(); ++i){
+        // m_fps_topic[i] = ros_api.fps_list[i].fps;
+        m_fps_topic_str[i] = all_header::to_string_p(ros_api.fps_list[i].fps, 1);
     }
     // m_currentTime = glutGet(GLUT_ELAPSED_TIME);
 	// if (m_currentTime - m_timebase > 1000)
