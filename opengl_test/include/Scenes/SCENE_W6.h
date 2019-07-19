8ebf11ac6b2403720d9641388159d9a6c1d70197
Chun-Feng Huang <benson516@hotmail.com>
2019-07-19 11:26:05 +0800

    Adjust the position of scenes

---
+++ opengl_test/include/Scenes/SCENE_W6.h
@@ -0,0 +1,56 @@
+#ifndef SCENE_W6_H
+#define SCENE_W6_H
+
+#include "Scene.h"
+
+//
+
+class SCENE_W6 : public Scene
+{
+public:
+	SCENE_W6(std::string pkg_path_in);
+
+private:
+    inline static bool cal_viewport_w(int w, int h, int &cx, int &cy, int &vw, int &vh){
+        double asp = 1.5833333333;
+        int im_w = w/7;
+        int im_h = int(im_w/asp);
+        cx = im_w*6;
+        cy = 0;
+        vw = im_w;
+        vh = im_h;
+        return true;
+    }
+};
+
+
+
+SCENE_W6::SCENE_W6(std::string pkg_path_in)
+{
+	_camera_ptr.reset(new ViewManager());
+    // _camera_ptr->assign_cal_viewport(&cal_viewport_w);
+    // Layout
+    //----------------------------------------//
+    attach_cal_viewport_func_ptr(0, &cal_viewport_w);
+    switch_layout(0);
+    //----------------------------------------//
+
+    _pkg_path = (pkg_path_in);
+    _Assets_path = (pkg_path_in + "Assets/");
+
+
+
+
+
+    // Back ground image rmImageDynamicBackground
+    // std::shared_ptr<rmImageDynamicBackground> _image_background_2_ptr(new rmImageDynamicBackground(_Assets_path, int(MSG_ID::camera_2)) );
+    std::shared_ptr<rmImageBoard> _image_background_2_ptr(new rmImageBoard(_Assets_path, int(MSG_ID::camera_right_rear), false, false, true) );
+    _image_background_2_ptr->alpha = 1.0;
+    _image_background_2_ptr->color_transform = glm::vec4(1.0f);
+    _rm_BaseModel.push_back( _image_background_2_ptr );
+
+
+
+}
+
+#endif  // SCENE_W6_H
