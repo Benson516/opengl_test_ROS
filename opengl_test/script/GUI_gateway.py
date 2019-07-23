fe125ee317752dd2b2a13ac9e4005bed92a6f396
Chun-Feng Huang <benson516@hotmail.com>
2019-07-23 16:44:08 +0800

    Add MQTT support

--- opengl_test/script/GUI_gateway.py
+++ opengl_test/script/GUI_gateway.py
@@ -10,17 +10,104 @@ from std_msgs.msg import Empty
 from opengl_test.msg import * # test
 
 
-# Publishers
+# MQTT
+#---------------------------#
+import paho.mqtt.client as mqtt
+#---------------------------#
+
+
+# ROS Publishers
 msg_recorder_trigger_pub = rospy.Publisher('/REC/req_backup', Empty, queue_size=10)
 GUI_cmd_pub = rospy.Publisher('GUI2/operation', GUI2_op, queue_size=10)
 GUI_cmd_pub_seq = 0
 
-# Responce
+# Global variables
+#----------------------------#
+# GUI_op responce
 GUI_state = GUI2_op()
 GUI_state_seq = 0
 GUI_state_seq_old = 0
+#----------------------------#
 
 
+# MQTT
+#------------------------------------------------------------#
+mqtt_client = None
+mqtt_broker = "127.0.0.1" # localhost
+# MQTT topics
+mqtt_msg_topic_namespace = 'REC'
+
+# Subscribed topics (MQTT --> ROS)
+#------------------------#
+# Alive signal
+mqtt_REC_req_backup_subT = mqtt_msg_topic_namespace + '/req_backup'
+
+# The subscription list
+# element: (topic, QoS)
+mqtt_subscription_list = list()
+# NOTE: since that we are subscrbing to local broker, the loading is tiny for QoS=2
+mqtt_subscription_list.append( (mqtt_REC_req_backup_subT, 2) )
+#------------------------#
+
+# Published topics (MQTT --> dock)
+#------------------------#
+mqtt_sync_pubT = mqtt_msg_topic_namespace + '/sync' #
+#------------------------#
+
+#------------------------------------------------------------#
+# end MQTT
+
+
+
+
+
+# MQTT --> ROS
+#------------------------------------------------------#
+# The callback for when the client receives a CONNACK response from the server.
+def mqtt_on_connect(client, userdata, flags, rc):
+    """
+    This is the callback function at connecting the MQTT client.
+    """
+    global mqtt_luggage_dock_namespace
+
+    rospy.loginfo("MQTT client connected with result code <" + str(rc) + ">.")
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe( mqtt_subscription_list )
+
+# The callback for when a PUBLISH message is received from the server.
+'''
+def mqtt_on_message(client, userdata, mqtt_msg):
+    """
+    This is the callback function at MQTT messages (un-matched topics) arrival.
+    ** On receiving this message, bypass to ROS.
+    """
+    print(mqtt_msg.topic + " " + str(mqtt_msg.payload))
+'''
+
+def mqtt_REC_req_backup_CB(client, userdata, mqtt_msg):
+    """
+    This is the callback function at receiving req_backup signal.
+    ** On receiving this message, bypass to ROS.
+    """
+    msg_recorder_trigger_pub.publish( Empty() )
+#------------------------------------------------------#
+# end MQTT --> ROS
+
+# ROS callbacks
+#------------------------------------------------------------#
+def GUI2_state_CB(msg):
+    global GUI_state, GUI_state_seq
+    GUI_state = msg
+    GUI_state_seq += 1
+    print("[GUI-gateway] GUI state recieved.")
+#------------------------------------------------------------#
+# end ROS callbacks
+
+
+# HTTP server
+#------------------------------------------------------------#
 class GUI_GATEWAY(object):
     @cherrypy.expose
     def index(self):
@@ -82,21 +169,28 @@ class GUI_GATEWAY(object):
         res_data["image_surr"] = GUI_state.image_surr
         # jdata = json.dumps(data)
         return res_data
+#------------------------------------------------------------#
+# end HTTP server
 
 
 
-def GUI2_state_CB(msg):
-    global GUI_state, GUI_state_seq
-    GUI_state = msg
-    GUI_state_seq += 1
-    print("[GUI-gateway] GUI state recieved.")
 
+
+
+
+
+
+
+
+# Wait for ROS terminating signal to close the HTTP server
 def wait_for_close():
     while not rospy.is_shutdown():
         rospy.sleep(0.5)
     cherrypy.engine.exit()
 
-if __name__ == '__main__':
+def main():
+    #
+    global mqtt_client
     # ROS
     rospy.init_node('GUI_gateway', anonymous=True)
     rospy.Subscriber("GUI2/state", GUI2_op, GUI2_state_CB)
@@ -105,6 +199,19 @@ if __name__ == '__main__':
     # _t.daemon = True
     _t.start()
 
+
+    # MQTT
+    #-------------------------------------------------------------------#
+    mqtt_client = mqtt.Client()
+    # Callback functions
+    mqtt_client.on_connect = mqtt_on_connect
+    # mqtt_client.on_message = mqtt_on_message # We don't need to process the message we don't want
+
+    # Subscriber callbacks
+    mqtt_client.message_callback_add(mqtt_REC_req_backup_subT, mqtt_REC_req_backup_CB)
+    #-------------------------------------------------------------------#
+
+
     # HTTP server
     cherrypy.server.socket_host = '0.0.0.0'
     cherrypy.server.socket_port = 6060
@@ -112,3 +219,6 @@ if __name__ == '__main__':
     cherrypy.quickstart(GUI_GATEWAY())
 
     print("End of GUI_gateway")
+
+if __name__ == '__main__':
+    main()
