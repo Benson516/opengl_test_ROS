6533f9b2d80adeadded15fbeffb6d53a93cfd772
benson516 <benson516@hotmail.com>
2020-05-14 18:18:38 +0800

    Update SCENE_IM_ONE.h

--- opengl_test/include/Scenes/SCENE_IM_ONE.h
+++ opengl_test/include/Scenes/SCENE_IM_ONE.h
@@ -1,5 +1,5 @@
-#ifndef SCENE_IM00_H
-#define SCENE_IM00_H
+#ifndef SCENE_IM_ONE_H
+#define SCENE_IM_ONE_H
 
 #include "Scene.h"
 
@@ -8,18 +8,23 @@
 #include "GUI_version_control.h"
 //----------------------------------------//
 
-class SCENE_IM00 : public Scene
+class SCENE_IM_ONE : public Scene
 {
 public:
-	SCENE_IM00(std::string pkg_path_in);
+	SCENE_IM_ONE(std::string pkg_path_in);
 
 private:
     inline static bool cal_viewport_w(int w, int h, int &cx, int &cy, int &vw, int &vh){
         double asp = _IMAGE_ASP_;
-        int im_h = h/3;
-        int im_w = int(im_h*asp);
-        cx = im_w*0;
-        cy = im_h*(2-0);
+        int im_w = w; // int(im_h*asp);
+        int im_h = h;
+        if (im_h*asp > im_w){
+            im_h = int(im_w/asp);
+        }else{
+            im_w = int(im_h*asp);
+        }
+        cx = 0;
+        cy = 0;
         vw = im_w;
         vh = im_h;
         return true;
@@ -28,13 +33,13 @@ private:
 
 
 
-SCENE_IM00::SCENE_IM00(std::string pkg_path_in)
+SCENE_IM_ONE::SCENE_IM_ONE(std::string pkg_path_in)
 {
 	_camera_ptr.reset(new ViewManager());
     // _camera_ptr->assign_cal_viewport(&cal_viewport_w);
     // Layout
     //----------------------------------------//
-    attach_cal_viewport_func_ptr(0, &cal_viewport_w);
+    attach_cal_viewport_func_ptr(1, &cal_viewport_w);
     switch_layout(0);
     //----------------------------------------//
 
@@ -53,18 +58,11 @@ SCENE_IM00::SCENE_IM00(std::string pkg_path_in)
     _image_background_2_ptr->alpha = 1.0;
     _image_background_2_ptr->color_transform = glm::vec4(1.0f);
     _rm_BaseModel.push_back( _image_background_2_ptr );
-
-
-    // Bounding box
-    // _box2D_ptr.reset(new rmBoundingBox2D(_Assets_path, int(MSG_ID::bounding_box_image_left_fore), false, false ) );
-    // _box2D_ptr->setup_params(_IMAGE_W_, _IMAGE_H_, 0, 0);
-    // // _box2D_ptr->alpha = 0.7;
-    // _rm_BaseModel.push_back( _box2D_ptr );
-
+    // Bounding box with tag
     _box2Dtag_ptr.reset(new rmlv2TagBoundingBox2D(_Assets_path, int(MSG_ID::bounding_box_image_left_fore), false, false ) );
     _box2Dtag_ptr->setup_params(_IMAGE_W_, _IMAGE_H_, 0, 0);
     _rm_BaseModel.push_back( _box2Dtag_ptr );
 
 }
 
-#endif  // SCENE_IM00_H
+#endif  // SCENE_IM_ONE_H
